e45a377aaa2d6f4e55b99e075b690cd11d94e423
chenxinsui <xinsui.chen@kreditme.id>
2021-02-25 15:56:32 +0800

    添加忽略

---
+++ demo-springstatemachine/src/main/java/com/example/demo/springstatemachine/DemoSpringstatemachineApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.springstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringstatemachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringstatemachineApplication.class, args);
+    }
+
+}
---
+++ demo-springstatemachine/src/test/java/com/example/demo/springstatemachine/DemoSpringstatemachineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo.springstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringstatemachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println(123456);
+    }
+
+
+
+}
